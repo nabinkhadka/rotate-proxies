b16f58965c2b72a59c49ce95aac41669a22cae30
nkhadka <nbnnbnkhadka@gmail.com>
2019-10-13 21:28:20 +0545

    Makes ready

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import sys
+import setuptools
 from distutils.core import setup
 
 setup(name='rotate-proxies',
@@ -7,6 +8,7 @@ setup(name='rotate-proxies',
         description='Scrapy Proxies: random proxy middleware for Scrapy',
         author='Nabin Khadka',
         author_email='nbnkhadka14@gmail.com',
-        url='https://github.com/nabinkhadka/scrapy-proxies',
+        url='https://github.com/nabinkhadka/rotate-proxies',
         packages=['rotate_proxies'],
+        setup_requires=['wheel']
         )
